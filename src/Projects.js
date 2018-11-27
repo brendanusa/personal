0affe92ac60bfd190bd6af8e4d44a116d172a49c
Brendan Bansavage <brendaniscool@gmail.com>
2018-11-26 16:16:39 -0800

    add Projects view

---
+++ src/Projects.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Projects.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Projects">
+        <div className="left">
+        </div>
+        <div className="center">
+        </div>
+        <div className="right">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Projects;
