24feca54f1d75f802d2ec24d9b74540689e2bf64
Brendan Bansavage <brendaniscool@gmail.com>
2018-11-27 23:56:29 -0800

    add routes and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Projects from './Projects';
+import Contact from './Contact';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Router>
+        <div>
+          <Header />
+          <Route path='/' exact component={Home}/>
+          <Route path='/home' component={Home}/>
+          <Route path='/projects' component={Projects}/>
+          <Route path='/contact' component={Contact}/>
+        </div>
+        </Router>
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import './Header.css';
+        
+// SELECT TEAMS LINK
+// <span>
+//   <Link to={'/teamselect'} style={{marginRight:"40px"}}>SELECT TEAMS</Link>
+// </span>
+
+const Header = () => (
+  <div className="Header">
+    <div className="link">
+      <Link to={'/home'}>
+        <span>HOME</span>
+      </Link>
+      <Link to={'/projects'}>
+        <span>PROJECTS</span>
+      </Link>
+      <Link to={'/contact'}>
+        <span>CONTACT</span>
+      </Link>
+    </div>
+  </div>
+)
+
+export default Header;
