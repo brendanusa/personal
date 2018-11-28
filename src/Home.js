6a3fe85cab1762ff8370295cceaa615c595b2950
Brendan Bansavage <brendaniscool@gmail.com>
2018-11-28 15:19:02 -0800

    complete home component

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+
+  return (
+    <div className="Home">
+      <p>
+        Thank you for visiting! Check out my projects and awards or send me a message using the links above. Or just enjoy this gloomy photo of Alcatraz.
+      </p>
+    </div>
+  )
+
+}
+
+export default Home;
