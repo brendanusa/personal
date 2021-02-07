c1536d4d1563eaebc51798860afce585284f03be
Brendan Bansavage <brendaniscool@gmail.com>
2021-02-07 08:37:31 -0800

    add happy bday page for bill

---
+++ src/Dad.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Dad.css';
+
+const Dad = () => {
+
+  return (
+    <div className="Dad">
+      <h2>HAPPY BIRTHDAY DAD!</h2>
+      <iframe width="346" height="312" src="https://www.youtube.com/embed/MVvA8ORn5hc?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h2>Hope you have a great day!</h2>
+      <div>
+        <p>Love,<br />Brendan</p>
+      </div>
+    </div>
+  )
+
+}
+
+export default Dad;
