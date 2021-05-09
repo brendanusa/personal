bc53cb592466734027e16561b3eac6bf16b04690
Brendan Bansavage <brendaniscool@gmail.com>
2021-05-09 10:36:01 -0700

    update mom page for Mday

--- src/Mom.js
+++ src/Mom.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import './Mom.css';
+import React from "react";
+import "./Mom.css";
 
 const Mom = () => {
-
   return (
     <div className="Mom">
-      <h2>HAPPY BIRTHDAY MOM!</h2>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Q1hIl5Lo_Xo?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <h2>Miss you and hope you're having a great day!</h2>
+      <h2>HAPPY MOTHER'S DAY!</h2>
+      <iframe
+        width="360"
+        height="480"
+        src="https://www.youtube.com/embed/cdaD7rpsAr4?controls=0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      />
+      <h2>Sorry for the choppy video!</h2>
       <div>
-        <p>Love,<br />Brendan</p>
+        <a href="https://youtu.be/QM9Yvlr_klE" target="_blank">
+          <h1>You can check out the original song here</h1>
+        </a>
+      </div>
+      <div>
+        <p>
+          Love,
+          <br />
+          Brendan
+        </p>
       </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default Mom;
